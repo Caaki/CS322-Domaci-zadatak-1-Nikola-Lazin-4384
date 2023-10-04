7f7c1a6d650630d341125db74bfe105612cdce9a
student <student@RU2S22>
2023-10-04 17:07:47 +0200

    Add project files.

---
+++ CS322-Domaci-zadatak-1-Nikola-Lazin-4384/Form1.cs
@@ -0,0 +1,25 @@
+namespace CS322_Domaci_zadatak_1_Nikola_Lazin_4384
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CS322-Domaci-zadatak-1-Nikola-Lazin-4384/Program.cs
@@ -0,0 +1,17 @@
+namespace CS322_Domaci_zadatak_1_Nikola_Lazin_4384
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
